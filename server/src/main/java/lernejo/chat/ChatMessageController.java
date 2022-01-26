dc44a4010d077d407a081bd890acc75f96faf1e4
Lagia666 <91790141+Lagia666@users.noreply.github.com>
2022-01-26 16:08:44 +0100

    Maven multi-module

--- server/src/main/java/lernejo/chat/ChatMessageController.java
+++ server/src/main/java/lernejo/chat/ChatMessageController.java
@@ -1,9 +1,8 @@
-package fr.lernejo.chat;
+package lernejo.chat;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
